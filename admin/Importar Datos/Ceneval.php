5c60ef0aea961f5943edd1c5abd55ae3f4642f9c
David Ojeda <ojedacortes1@gmail.com>
2022-06-08 21:05:22 -0500

    Frontend para importar datos de SISENI
    Se añadieron 3 páginas principales que contienen un tipo de importación de datos diferentes, junto con el archivo de estilos compatible con los demás módulos .scss y se genero a partir de él los archivos .css y .css.map

---
+++ admin/Importar Datos/Ceneval.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Importar datos del Ceneval</title>	
+	<link rel="stylesheet" href="/build/css/importar.css">
+</head>
+<body>
+    <?php //Metódo de header  
+        require "../includes/funciones.php";
+        inlcuirTemplate('header');
+    ?>
+	<div class="contenedor">
+		<div class="enunciado">
+            <p>Importar datos del Ceneval</p>
+        </div>
+		<br>
+		<p>Importar Archivo: </p>
+		<div class="funciones">
+			
+			<label for="importA">Selecciona el archivo</label>
+			<input class = "archivo" type="file" name = "importA" id="importA">
+			<h4 id="nombre"></h4>
+			<br>
+			<br>
+			<button id="btnImport">Importar datos</button>
+		</div>
+		
+	</div>
+	<script type="text/javascript">
+		let archivo = document.querySelector('#importA');
+		archivo.addEventListener('change', () => {
+			document.querySelector('#nombre').innerText =
+				archivo.files[0].name;
+		});
+	</script>
+    <?php //Metódo de footer
+        inlcuirTemplate('footer');
+    ?>
+</body>
+</html>
