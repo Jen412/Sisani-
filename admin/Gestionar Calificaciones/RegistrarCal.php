5cdf048c096eeb72eb6f68873b5044da73ab74e6
Jen412 <72884006+Jen412@users.noreply.github.com>
2022-06-15 11:59:51 -0500

    Agregar detalles

--- admin/Gestionar Calificaciones/RegistrarCal.php
+++ admin/Gestionar Calificaciones/RegistrarCal.php
@@ -4,6 +4,20 @@
         header('location: /');
     }
     inlcuirTemplate('header');
+    require "../../includes/config/database.php";
+    $db =conectarDB();
+    $queryMat ="SELECT * FROM materias";
+    $resultadoMat =mysqli_query($db, $queryMat);
+
+    $grupo="";
+    $materia="";
+    $carrera="";
+
+    if ($_SERVER['REQUEST_METHOD']=="POST") {
+        $grupo = $_POST['GrupoS'];
+        $materia=$_POST['materiaS'];
+        $carrera=$_POST['carreraS'];
+    }
 ?>
 <main class="registrarCal">
     <section>
@@ -12,7 +26,7 @@
             <div class="linea">
                 <div class="carrera">
                     <label for="">Selecciona Carrera</label>
-                    <select name="carreraS" id="carreraS">
+                    <select name="v" id="carreraS">
                         <option value="" disabled selected>--Seleccione Carrera--</option>    
                     </select>
                 </div>
@@ -20,6 +34,9 @@
                     <label for="">Selecciona Materia</label>
                     <select name="materiaS" id="materiaS">
                         <option value="" disabled selected>--Seleccione Materia--</option>    
+                        <?php while($materia = mysqli_fetch_assoc($resultadoMat)):?>
+                            <option value="<?php echo $materia['idMateria'];?>"><?php echo $materia['nombre_Mat'];?></option>
+                        <?php endwhile;?>
                     </select>
                 </div>
                 <div class="grupo">
@@ -28,7 +45,7 @@
                         <option value="" disabled selected>--Seleccione Grupo--</option>    
                     </select>
                 </div>
-                <input type="button" value="Buscar">
+                <input type="submit" value="Buscar">
             </div>
         </form>
     </section>
